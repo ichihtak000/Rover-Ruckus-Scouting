8f049c4961402bb867c430a94d60872794e9f727
Takumi <takumiichihashi@gmail.com>
2018-11-25 18:37:20 -0800

    formating done

--- test.php
+++ test.php
@@ -1,17 +1,30 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h1>My first PHP page</h1>
-
+<!--
 <?php
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+
+$fn = "fff.html";
+$myfile = fopen(fn, "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 $txt = "Jane Doe\n";
 fwrite($myfile, $txt);
 fclose($myfile);
+$myfile1 = fopen("new.html", "r") or die("Unable to open file!");
+echo fread($myfile1,filesize("new.html"));
+fclose($myfile1);
+
 ?>
+-->
+</br>
+
+fileName <?php echo $_GET["fileName"]; ?><br> 
+Red1 <?php echo $_GET["red1"]; ?><br>
+Red2 <?php echo $_GET["red2"]; ?><br>
+
+Blue1 <?php echo $_GET["blue1"]; ?><br>
+Blue2 <?php echo $_GET["blue2"]; ?><br>
 
 </body>
 </html>
