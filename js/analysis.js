d3e0185736bf72caa4209f7d0d41016d117bed89
Takumi <takumiichihashi@gmail.com>
2018-12-15 02:07:44 -0800

    Interleague 2:07pm

--- js/analysis.js
+++ js/analysis.js
@@ -113,6 +113,9 @@ function updateTable2(){
 	var totalHigh;
 	var totalLow;
 	var standardDev;
+	var autoAve;
+	var teleAve;
+	var endAve;
 	
 	var numTeam = 0;
 	var numTeamEach = 0;
@@ -121,12 +124,18 @@ function updateTable2(){
 	var totalAveScore = 0;
 	var highestTotal = 0;
 	var lowestTotal = Number.MAX_SAFE_INTEGER;
-	
+	var autoAveScore = 0;
+	var teleAveScore = 0;
+	var endAveScore = 0;
 	
 	for(var i=0; i<tLength; i++){
 		totalAveScore+= totalTable[i];
 		totalRanking += +rankingTable[i]; 
 		totalTie += +tieTable[i];
+		autoAveScore += +autoTable[i];
+		teleAveScore += +teleTable[i];
+		endAveScore += +endTable[i];
+		
 		numTeamEach++;
 		if(highestTotal < totalTable[i]){
 			highestTotal = totalTable[i];
@@ -145,17 +154,43 @@ function updateTable2(){
 			totalHigh = row.insertCell(4);
 			totalLow = row.insertCell(5);
 			standardDiv = row.insertCell(6);
+			autoAve = row.insertCell(7);
+			teleAve = row.insertCell(8);
+			endAve = row.insertCell(9);
 		}
 		if(!(+teamTable[i] == +teamTable[i+1])){
 			team.innerHTML = teamTable[i];
 			
+			var t = teamTable[i];
+			if(t == 5446){
+				totalRanking += 20;
+			}else if(t == 5604 || t == 5961 || t == 9876){
+				totalRanking += 16;
+			}else if(t == 11865 || t == 4054 || t == 11121 || t == 12217 || t == 13485 || t == 10244){
+				totalRanking += 14;
+			}else if(t == 6559 || t == 15460 || t == 7742 || t == 7732 || t == 8548 || t == 8563){
+				totalRanking +=12;
+			}else if(t == 5971 || t == 13601 || t == 3861 || t == 4213){
+				totalRanking +=10;
+			}else if(t == 11477 || t == 9387 || t == 6424 || t == 8030 || t == 10548){
+				totalRanking += 8;
+			}else if(t == 11186 || t == 7766 || t == 6046 || t == 5447 || t == 7830 || t == 7299){
+				totalRanking +=6;
+			}else if(t == 4053 || t == 6395 || t == 13480 || t == 15462 || t == 13707 || t == 13481){
+				totalRanking +=4;
+			}else if(t == 13484 || t == 15495){
+				totalRanking +=2;
+			}
+			
 			rank.innerHTML = totalRanking;
 			tie.innerHTML = totalTie;
-			
 			totalAve.innerHTML = Math.round(totalAveScore/numTeamEach);
 			totalHigh.innerHTML = highestTotal;
 			totalLow.innerHTML = lowestTotal;
 			standardDiv.innerHTML = endTable[i];
+			autoAve.innerHTML = Math.round(autoAveScore/numTeamEach);
+			teleAve.innerHTML = Math.round(teleAveScore/numTeamEach);
+			endAve.innerHTML = Math.round(endAveScore/numTeamEach);
 			
 			team.style.backgroundColor = color;
 			rank.style.backgroundColor = color;
@@ -164,14 +199,20 @@ function updateTable2(){
 			totalHigh.style.backgroundColor = color;
 			totalLow.style.backgroundColor = color;
 			standardDiv.style.backgroundColor = color;
+			autoAve.style.backgroundColor = color;
+			teleAve.style.backgroundColor = color;
+			endAve.style.backgroundColor = color;
 			
 			numTeam++;
 			totalRanking = 0;
 			totalTie = 0;
 			totalAveScore = 0;
+			autoAveScore = 0;
+			teleAveScore = 0;
+			endAveScore = 0;
 			numTeamEach = 0;
 			highestTotal = 0;
-			lowestTotal = Number.MAX_SAFE_INTEGER;;
+			lowestTotal = Number.MAX_SAFE_INTEGER;
 		}
 	}
 }
